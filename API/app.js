cad3e59e7fdb007f064da98fb0779d4dd6116d6d
Pedro Zenha <pedrozenha12@gmail.com>
2019-10-01 11:47:15 +0100

    first Dev commit

--- API/app.js
+++ API/app.js
@@ -1,11 +1,85 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/event');
+require('dotenv/config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+const events = [];
 
-app.post('/', (req,res) => {
-  const 
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    type RootQuery {
+      events: [Event!]!
+    }
+
+    type RootMutation {
+      createEvent(eventInput: EventInput): Event
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+     return Event.find()
+      .then(events => {
+        return events.map(event => {
+          return { ...event._doc };
+        })
+      })
+      .catch(err => {
+        throw err;
+      });
+    },
+    createEvent: (args) => {
+      const event = new Event({
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: args.eventInput.price,
+        date: new Date(args.eventInput.date)
+      });
+      return event.save().then(result => {
+              console.log(result);
+              return {...result._doc};
+              }).catch(err => {
+                console.log(err);
+                throw err;
+      });
+    }
+  },
+  graphiql: true
+}));
+
+mongoose.connect(process.env.DB_CONNECTION).then(() => {
+  app.listen(4000,function(){
+    console.log('Listen on port 4000');
+  }
+  );
 })
+.catch(err => {
+  console.log(err);
+});
+
